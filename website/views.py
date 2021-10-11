4f4ef1d936ef0d31e7f39b1db22fcfa523a93673
SuyogKhanal5 <84912009+SuyogKhanal5@users.noreply.github.com>
2021-10-11 11:00:03 -0400

    Tutorial Part 4

--- website/views.py
+++ website/views.py
@@ -1,55 +1,97 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
-from flask.helpers import url_for
 from flask_login import login_required, current_user
-from .models import Post
+from .models import Post, User, Comment
 from . import db
 
 views = Blueprint("views", __name__)
 
-@views.route("") # Makes it so that if you got to / or /home you get the same
-@views.route("home")
+
+@views.route("/")
+@views.route("/home")
 @login_required
 def home():
     posts = Post.query.all()
-    return render_template("home.html", posts = posts, user = current_user) 
-    # create variable, can pass into html document
+    return render_template("home.html", user=current_user, posts=posts)
 
-# When creating html5 file, write html:5 then press tab
 
-# templates folder must be called templates
-
-@views.route("create-post", methods = ['GET', 'POST'])
+@views.route("/create-post", methods=['GET', 'POST'])
 @login_required
 def create_post():
-    if request.method == 'POST':
+    if request.method == "POST":
         text = request.form.get('text')
 
         if not text:
-            flash('Post cannot be empty', category = 'error')
+            flash('Post cannot be empty', category='error')
         else:
-            post = Post(text = text, author = current_user.id)
+            post = Post(text=text, author=current_user.id)
             db.session.add(post)
             db.session.commit()
-
-            flash('Post Created!', category = 'success')
-
+            flash('Post created!', category='success')
             return redirect(url_for('views.home'))
 
-    return render_template('create_post.html', user = current_user)
+    return render_template('create_post.html', user=current_user)
+
 
 @views.route("/delete-post/<id>")
 @login_required
 def delete_post(id):
-    post = Post.query.filter_by(id = id).first()
+    post = Post.query.filter_by(id=id).first()
 
     if not post:
-        flash("Post does not exist", category = 'error')
+        flash("Post does not exist.", category='error')
     elif current_user.id != post.id:
-        flash("You do not have permission to delete this post", category = 'error')
+        flash('You do not have permission to delete this post.', category='error')
     else:
         db.session.delete(post)
         db.session.commit()
-        flash("Post deleted", category = "success")
+        flash('Post deleted.', category='success')
 
     return redirect(url_for('views.home'))
-        
+
+
+@views.route("/posts/<username>")
+@login_required
+def posts(username):
+    user = User.query.filter_by(username=username).first()
+
+    if not user:
+        flash('No user with that username exists.', category='error')
+        return redirect(url_for('views.home'))
+
+    posts = user.posts
+    return render_template("posts.html", user=current_user, posts=posts, username=username)
+
+@views.route("/create-comment/<post_id>", methods=['POST'])
+@login_required
+def create_comment(post_id):
+    Text = request.form.get('text')
+
+    if not Text:
+        flash('Comment cannot be empty.', category='error')
+    else:
+        post = Post.query.filter_by(id=post_id)
+        if post:
+            comment = Comment(text=Text, author=current_user.id, post_id=post_id)
+            db.session.add(comment)
+            db.session.commit()
+
+            flash('Comment was posted', category = "success")
+        else:
+            flash('Post does not exist.', category='error')
+
+    return redirect(url_for('views.home'))
+
+@views.route("/delete-comment/<comment_id>")
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash('Comment does not exist.', category='error')
+    elif current_user.id != comment.author and current_user.id != comment.post.author:
+        flash('You do not have permission to delete this comment.', category='error')
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+
+    return redirect(url_for('views.home'))
